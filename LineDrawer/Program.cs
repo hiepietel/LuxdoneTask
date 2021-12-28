c720d2fdf647a27fa1b64819058c5b0f96505065
hiepietel <hiepietel@MSI>
2021-12-28 15:55:43 +0100

    Add project files.

---
+++ LineDrawer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineDrawer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
