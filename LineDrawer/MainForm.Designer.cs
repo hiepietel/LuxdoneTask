fb3d4d51c11b7c65dc2b647c88bd2171815b292b
Jan Pietrzak <jan.pietrzak@galaxy.com.pl>
2021-12-28 18:13:52 +0100

    Simple Line Drawer

---
+++ LineDrawer/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace LineDrawer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.mainPanel.Size = new System.Drawing.Size(974, 522);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 522);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "MainForm";
+            this.Text = "LineDrawer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+    }
+}
+
---
+++ LineDrawer/MainForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LineDrawer
+{
+    public partial class MainForm : Form
+    {
+
+        Graphics g;
+        int x =-1;
+        int y = -1;
+        Pen pen;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            g = mainPanel.CreateGraphics();
+            pen = new Pen(Color.Black, 5);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if( x == -1 || y == -1)
+            {
+                x = e.X;
+                y = e.Y;
+                return;
+            }
+
+            var p = new Point()
+            {
+                X = x,
+                Y = y
+            };
+            var pNew = new Point()
+            {
+                X = e.X,
+                Y = e.Y
+            };
+            g.DrawLine(pen, p, pNew);
+            x = e.X;
+            y = e.Y;    
+        }
+    }
+}
