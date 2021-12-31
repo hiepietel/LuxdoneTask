64a319ba51a32365a6af75409ead3bbcd15a55f8
hiepietel <jachumail@yahoo.pl>
2021-12-31 15:46:25 +0100

    Static Extensions added.

---
+++ LineDrawer/Extensions/PointExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace LineDrawer.Extensions
+{
+    public static class PointExtensions
+    {
+        public static Point RescalePoint(this Point point)
+        {
+            int xe = point.X % 10;
+            point.X -= xe;
+
+            int ye = point.Y % 10;
+            point.Y -= ye;
+
+            return point;
+        }
+
+        public static Point CreateRescaledPoint(int x, int y)
+        {
+            var point = new Point(x, y);
+            return RescalePoint(point);
+        }
+    }
+}
