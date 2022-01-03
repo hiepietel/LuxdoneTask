69a4440f9886360060534544106643fa630f8f76
hiepietel <jachumail@yahoo.pl>
2022-01-03 17:52:52 +0100

    BezierPoint

---
+++ LineDrawer/Model/BezierPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace LineDrawer.Model
+{
+    public class BezierPoint
+    {
+        public Point FirstPoint { get; set; }
+        public Point C0 { get; set; }
+        public Point C1 { get; set; }
+        public Point SecondPoint { get; set; }
+    }
+}
